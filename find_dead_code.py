ff84e79842a40ed8ffb1d118c390833489b234d0
mike <mike@lcd-outlet.com>
2020-07-15 15:29:51 -0700

    check filetype

--- find_dead_code.py
+++ find_dead_code.py
@@ -1,10 +1,25 @@
-import pandas as pd 
-df = pd.read_csv('output.csv')
-df.head()
-function_names = set(df[df.type=='FunctionDef'].name.tolist())
-functions_called = set(df[df.type=='call'].name.tolist())
-len(function_names.difference(functions_called))
-for ix in function_names:
-    if ix not in functions_called:
-        if 'self.{}'.format(ix) not in functions_called:
-            print(ix)
+from sys import argv
+
+import pandas as pd
+
+#argv[1] filename of csv from ast_test.py
+
+def get_file_type(filename):
+    #https://stackoverflow.com/a/24073625
+    import subprocess
+    import shlex
+    cmd = shlex.split('file --mime-type {0}'.format(filename))
+    result = subprocess.check_output(cmd)
+    mime_type = result.split()[-1]
+    return mime_type
+
+file_type = get_file_type(argv[1])
+if file_type == b'application/csv':
+    df = pd.read_csv(argv[1])
+    function_names = set(df[df.type=='FunctionDef'].name.tolist())
+    functions_called = set(df[df.type=='call'].name.tolist())
+    len(function_names.difference(functions_called))
+    for ix in function_names:
+        if ix not in functions_called:
+            if 'self.{}'.format(ix) not in functions_called:
+                print(ix)
