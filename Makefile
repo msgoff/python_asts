9a12509817e933285b3655771b5b84ca7e3d7889
mike <mike@lcd-outlet.com>
2020-07-19 20:48:53 +0000

    makefile

--- Makefile
+++ Makefile
@@ -7,7 +7,11 @@ help:
 
 
 docker:
+	/bin/bash sparse_checkout_shallow_clone.sh
 	@cd ds/search_d3js_graph/;sudo service docker start;sudo su -c "docker build -t flask_test ." root; sudo su -c "docker run -it --rm  --publish 5000:5000 flask_test" root
+
+
+
 docmac:
 	docker build -t pyast .
 	docker run -it --rm pyast /bin/bash
--- sparse_checkout_shallow_clone.sh
+++ sparse_checkout_shallow_clone.sh
@@ -1,8 +1,14 @@
 #https://stackoverflow.com/a/28039894
-git init ds
-cd ds
-git remote add origin https://github.com/allofphysicsgraph/dynamic-search
-git config core.sparsecheckout true
-echo "search_d3js_graph/*" >> .git/info/sparse-checkout
-git pull --depth=1 origin master
-ls|grep py$|xargs -i cp {} ds/search_d3js_graph/
+if [[ ! -d ds/dynamic-search ]];
+then
+	git init ds
+	cd ds
+	git remote add origin https://github.com/allofphysicsgraph/dynamic-search
+	git config core.sparsecheckout true
+	echo "search_d3js_graph/*" >> .git/info/sparse-checkout
+	git pull --depth=1 origin master
+else
+	cd ds/dynamic-search
+	git pull
+
+fi
